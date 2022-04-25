44197a93e4e7146e540291bada529aaeb0c76cf6
Rakibulhasan70 <rakibulhasansohag5@gmail.com>
2022-04-25 23:04:58 +0600

    db

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = "mongodb+srv://carProject:HpOU2XQ8ToxTeJL5@cluster0.faflb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.faflb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
